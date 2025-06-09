71f06e8026c5c82ea2c5e4665657ca7663cb1f03
mattboostkit <matt@boostkit.io>
2025-06-09 20:07:09 +0100

    Latest Fixes

--- postcss.config.js
+++ postcss.config.js
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 module.exports = {
   plugins: {
     tailwindcss: {},
     autoprefixer: {},
   },
 };
-=======
-module.exports = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-  },
-};
->>>>>>> e5d647af0de7eeb4bee63671ae86a204aaeec73a
